d3ea27334967500601371cb3f1876e93bc6dede2
Jason <jasondye.music@gmail.com>
2022-06-03 15:50:26 -0500

    Remove MUI usage from MusicList.jsx

--- src/components/MusicList.jsx
+++ src/components/MusicList.jsx
@@ -2,16 +2,6 @@ import React, { useState } from 'react';
 import MusicListItem from './MusicListItem';
 import useStore from '../zustandStore';
 
-import Grid from '@mui/material/Grid';
-import Grow from '@mui/material/Grow';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import FormControl from '@mui/material/FormControl';
-import FormHelperText from '@mui/material/FormHelperText';
-import Select from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-
 export default function MusicList(props) {
 
   // global state
@@ -32,62 +22,47 @@ export default function MusicList(props) {
     filterMusicList(e.target.value);
   };
 
+  /*
+    <FormControl error fullWidth mb="15px">
+      <FormHelperText>Instrument</FormHelperText>
+      <InputLabel color="secondary" id="instrument-select">Instrument</InputLabel>
+      <Select
+        color="secondary"
+        labelId="instrument-filter"
+        id="instrument-filter"
+        value={instrument}
+        label="Instrument"
+        onChange={handleInstrumentSelectChange}
+      >
+        <MenuItem value={'Marimba'}>Marimba</MenuItem>
+        <MenuItem value={'Snare Drum'}>Snare Drum</MenuItem>
+        <MenuItem value={'Ensemble'}>Ensemble</MenuItem>
+      </Select>
+    </FormControl>
+  */
+
   return (
-    <Grid
-      container
-      item
-      px={'12px'}
-    >
-      <Box>
-        <Typography variant="h5" color="secondary" align="left" pb="15px">
-          Collection Title
-        </Typography>
-        {/* <FormControl error fullWidth mb="15px">
-          <FormHelperText>Instrument</FormHelperText>
-          <InputLabel color="secondary" id="instrument-select">Instrument</InputLabel>
-          <Select
-            color="secondary"
-            labelId="instrument-filter"
-            id="instrument-filter"
-            value={instrument}
-            label="Instrument"
-            onChange={handleInstrumentSelectChange}
-          >
-            <MenuItem value={'Marimba'}>Marimba</MenuItem>
-            <MenuItem value={'Snare Drum'}>Snare Drum</MenuItem>
-            <MenuItem value={'Ensemble'}>Ensemble</MenuItem>
-          </Select>
-        </FormControl> */}
-        <Grid
-          container
-          item
-          spacing={1}
-          justifyContent="start"
-          sx={{
-            height: '70%',
-            overflow: 'scroll'
-          }}
-        >
-          { musicList.map((exerpt, index) => {
+    <div id="music-list-cntnr" className="container">
+      <div className="box">
+        <div id="music_list-hdr-txt">
+          <h3 className="center">Collection Title</h3>
+        </div>
+        <ul id="music-list" className="container top">
+          {musicList.map((exerpt, index) => {
             return (
-              <Grow
-                in={true}
-                timeout={(index + 1) * 300}
-                style={{ transformOrigin: '0 0 0' }}
-                key={exerpt.title}
-              >
-                <Grid item xs={12}>
+              <li className="slide-in-fade-in" key={exerpt.title}>
+                <div style={{ width: '250px' }}>
                   <MusicListItem
                     index={index}
                     exerpt={exerpt}
                     handleListItemClick={handleListItemClick}
                   />
-                </Grid>
-              </Grow>
+                </div>
+              </li>
             );
           })}
-        </Grid>
-      </Box>
-    </Grid>
+        </ul>
+      </div>
+    </div>
   );
 }
