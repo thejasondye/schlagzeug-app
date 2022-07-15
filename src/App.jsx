7e49e2b05b81229e02c12da47436c4748b8fdcfa
Jason <jasondye.music@gmail.com>
2022-07-15 15:25:10 -0500

    Remove MUI func from App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,39 +7,13 @@ import MusicList from './components/MusicList';
 import AudioPlayer from './components/AudioPlayer';
 import NavBar from './components/NavBar';
 
-import { Typography, Container, Grid, Box, Paper } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#306678',
-      contrastText: '#9AB3BA',
-    },
-    secondary: {
-      main: '#9AB3BA'
-    },
-    warning: {
-      main: '#FDCB0B'
-    },
-    error: {
-      main: '#FDCB0B'
-    },
-  },
-  typography: {
-    fontFamily: "'Avenir', 'Georgia', 'Helvetica', sans-serif"
-  }
-});
-
-
-export default function App(props) {
+export default function App () {
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container disableGutters sx={{ height: '100vh', mx: '0px' }}>
-        <NavBar />
-        <Outlet />
-      </Container>
-    </ThemeProvider>
+    <div style={{ height: '100vh', width: '97vw', margin: '0px 10px' }}>
+      <NavBar />
+      <Outlet />
+    </div>
   );
 }
