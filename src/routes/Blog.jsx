173a86a9ff3f30dae5b673340e4c8d9629d37ea2
Jason <jasondye.music@gmail.com>
2022-07-15 14:40:25 -0500

    Remove MUI func from Blog page

--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -5,18 +5,15 @@ import { Link } from 'react-router-dom';
 
 import NavBar from '../components/NavBar';
 
-import { Typography, Container, Grid, Box, Paper } from '@mui/material';
-import textAlign from '@mui/system';
-
 
 export default function Blog(props) {
 
   return (
 
-    <Container>
-      <Grid item>
-        <Typography variant="h2">This is the Blog Page</Typography>
-      </Grid>
-    </Container>
+    <div className="container">
+      <div className="item">
+        <h2 variant="h2">This is the Blog Page</h2>
+      </div>
+    </div>
   );
 };
