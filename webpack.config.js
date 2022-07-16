aa77e46d2c0d5ace2ee833f97aa2abbb4035186e
Jason <jasondye.music@gmail.com>
2022-07-15 22:42:56 -0500

    uncomment style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          // { loader: 'style-loader' },
+          { loader: 'style-loader' },
           { loader: 'css-loader' }
         ]
       },
